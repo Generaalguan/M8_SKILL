c9f68369d5fc670a69927acba14446020a6ad2d2
Generaalguan <33372@ma-web.nl>
2023-06-22 09:28:31 +0200

    bijna klaar eindopdracht

--- encriptor/src/js/Renderer/Renderer.js
+++ encriptor/src/js/Renderer/Renderer.js
@@ -1,9 +1,10 @@
+
+
+
+
 class Renderer {
     render(whatToRender, whereToRender) {//de render functie krijgt 2 dingen binnen, wat hij moet renderen en waar hij moet renderen
         whereToRender.appendChild(whatToRender);//als hij dat binnen heeft dan gaat hij de whattorender in de wheretorender gooien
     }
 
 }
-
-
-
